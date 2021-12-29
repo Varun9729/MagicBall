414defd0dcf01685e1cd9311f7c0048e83a0625d
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2021-12-29 14:27:54 -0800

    Initial Commit

---
+++ Shared/MagicBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicBallApp.swift
+//  Shared
+//
+//  Created by Varun Kashyap on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MagicBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
